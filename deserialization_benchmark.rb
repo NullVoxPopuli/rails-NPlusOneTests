a83a61aa171e697d2a1cb001836e90eccecd246e
Preston Sego <LPSego3+dev@gmail.com>
2017-09-18 20:28:34 -0400

    deserialization fixed

--- deserialization_benchmark.rb
+++ deserialization_benchmark.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'benchmark/ips'
 require 'awesome_print'
 require 'active_support'
@@ -14,6 +16,40 @@ benchmarks = %i[ips memory]
 require './config/environment'
 
 GC.disable
+
+def custom_parsing(json)
+  result = {}
+  result[:id] = json['id'] if json['id']
+
+  result.merge!(json['attributes'])
+
+  json['relationships'].each do |relation_name, v|
+    data = v['data']
+    if data.is_a?(Array)
+      result[relation_name] = data.map do |relationship|
+        {
+          "#{relation_name.singularize}_id" => relationship[:id],
+          "#{relation_name.singularize}_type" => relationship[:type]
+        }
+      end
+    else
+      result["#{relation_name}_id"] = data['id']
+      result["#{relation_name}_type"] = data['type']
+    end
+  end
+
+  result
+end
+
+# Disable Validation
+module ActiveModelSerializers::Adapter::JsonApi::Deserialization
+  class << self
+    def validate_payload(_document)
+      # TODO: modify this to accept embedded documents
+      true
+    end
+  end
+end
 
 hash = {
   data: {
@@ -43,13 +79,13 @@ hash = {
     },
     relationships: {
       author: { data: { id: 1, type: 'users' } },
-      comments: {
-        data: [
-          { id: 1, type: 'comments' },
-          { id: 2, type: 'comments' },
-          { id: 3, type: 'comments' }
-        ]
-      }
+      # comments: {
+      #   data: [
+      #     { id: 1, type: 'comments' },
+      #     { id: 2, type: 'comments' },
+      #     { id: 3, type: 'comments' }
+      #   ]
+      # }
     }
   }
 }
@@ -66,12 +102,25 @@ jsonapi_deserialize = lambda {
   JSONAPI::Rails::Deserializable::Resource.call(data)
 }
 
+custom = lambda {
+  custom_parsing(data)
+}
+
+ap 'outputs'
+ap 'ams'
+ap ams_deserialize.call
+ap 'jsonapi'
+ap jsonapi_deserialize.call
+ap 'custom'
+ap custom.call
+
 benchmarks.each do |bench|
   Benchmark.send(bench) do |x|
     x.config(time: 10, warmup: 5, stats: :bootstrap, confidence: 95) if x.respond_to?(:config)
 
     x.report('ams       ') { ams_deserialize.call }
     x.report('jsonapi-rb') { jsonapi_deserialize.call }
+    x.report('custom    ') { custom.call }
 
     x.compare!
   end
